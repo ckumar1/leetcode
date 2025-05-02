cefb25d1458626f08a398fc21eab9f723161a893
Cherub Kumar <cherub.chopra@gmail.com>
2025-05-02 01:21:48 -0500

    Use helper method for tree construction
    Also uses a hash to look up location of root node in the inorder array

--- 105.construct-binary-tree-from-preorder-and-inorder-traversal.py
+++ 105.construct-binary-tree-from-preorder-and-inorder-traversal.py
@@ -11,23 +11,44 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+# from typing import List, Optional
+
+
 class Solution:
     def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
+        def dfs(preorder, inorder):
         
-        if len(preorder) == 0:
-            return None
-        root = TreeNode(preorder[0])
-        if len(preorder) == 1:
+            if len(preorder) == 0:
+                return None
+            root = TreeNode(preorder[0])
+            if len(preorder) == 1:
+                return root
+
+
+            i = inorder_map[root.val]
+
+            inorder_left, inorder_right = inorder[:i], inorder[i+1:]
+            preorder_left, preorder_right = preorder[1:i+1], preorder[i+1:]
+
+            root.left = dfs(preorder_left, inorder_left)
+            root.right = dfs(preorder_right, inorder_right)
+
             return root
 
-        i = inorder.index(root.val)
+        inorder_map = {v: i for i, v in enumerate(inorder)}
+        root_index = inorder_map[preorder[0]] 
 
-        inorder_left, inorder_right = inorder[:i], inorder[i+1:]
-        preorder_left, preorder_right = preorder[1:i+1], preorder[i+1:]
+        return dfs(preorder, inorder)
 
-        root.left = self.buildTree(preorder_left, inorder_left)
-        root.right = self.buildTree(preorder_right, inorder_right)
+    # def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
+    #     def buildt(preorder, inorder):
+    #         root_index = inorder_map[preorder[0]]
 
-        return root
+    #     inorder_map = {}
+    #     for i, v in enumerate(inorder):
+    #         inorder_map[v] = i
 
+    #     left_inorder, right_inorder = (0, i), (i+1, len(inorder) - 1)
+    #     left_preorder, right_preorder = (1, i+1), (i+2)
+    #     root = TreeNode(preorder[0], buildt(), buildt())
 # @lc code=end
